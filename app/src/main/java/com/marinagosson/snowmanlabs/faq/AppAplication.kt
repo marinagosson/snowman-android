477a8e55c13d720b5cd918b95f59c78ab0607a11
Marina Gosson <marina.gosson@gmail.com>
2020-11-06 17:42:30 -0300

    criacao da application

--- app/src/main/java/com/marinagosson/snowmanlabs/faq/AppAplication.kt
+++ app/src/main/java/com/marinagosson/snowmanlabs/faq/AppAplication.kt
@@ -1,2 +1,13 @@
 package com.marinagosson.snowmanlabs.faq
 
+import android.app.Application
+
+class AppAplication  : Application() {
+
+    private val TAG = AppAplication::class.java.simpleName
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
