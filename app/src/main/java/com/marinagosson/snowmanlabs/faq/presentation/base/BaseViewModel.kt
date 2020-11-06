a0218f4970ead4be749a5e096932887a5ec5cd0e
Marina Gosson <marina.gosson@gmail.com>
2020-11-06 17:46:31 -0300

    criado a base view model com coroutine

---
+++ app/src/main/java/com/marinagosson/snowmanlabs/faq/presentation/base/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.marinagosson.snowmanlabs.faq.presentation.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+
+abstract class BaseViewModel : ViewModel() {
+    private val scope = CoroutineScope(
+        Job() + Dispatchers.Main
+    )
+
+    override fun onCleared() {
+        super.onCleared()
+        scope.cancel();
+    }
+}
